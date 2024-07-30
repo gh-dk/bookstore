45ef986dd4aa5ab6141cb104b2f59129f66c998c
deepak kanojiya <deepak.kanojiya@neosoftmail.com>
2024-07-30 11:30:21 +0530

    hosting bug 2

--- assets/fetchBooks.js
+++ assets/fetchBooks.js
@@ -1,5 +1,5 @@
 async function fetchBooks(){
-        const response = await fetch('practise/books.json');
+        const response = await fetch('https://gh-dk.github.io/practise/books.json');
         const data = await response.json();
         return data;
 }
