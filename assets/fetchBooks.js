ff81ff5d4c308f84e6524844d4fc6ad6eb376591
deepak kanojiya <deepak.kanojiya@neosoftmail.com>
2024-07-30 11:19:21 +0530

    bug fixes

--- assets/fetchBooks.js
+++ assets/fetchBooks.js
@@ -1,5 +1,5 @@
 async function fetchBooks(){
-        const response = await fetch('../book-store/books.json');
+        const response = await fetch('../practise/books.json');
         const data = await response.json();
         return data;
 }
